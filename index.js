3a9ec0ae0a647c11a00f44217aa17203af1adfcb
Ashna Shah-Grover <as19@williams.edu>
2019-08-26 14:20:16 -0400

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  const hour = parseInt(time.substring(0, 2));
+  if (hour < 12) {return "Good Morning"}
+  if (hour > 17) {return "Good Evening"}
+  return "Good Afternoon"
+}
+
+function displayMessage(string){
+  document.querySelector("#greeting").innerText = string
+}
